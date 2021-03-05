981bcdda3e06188c70e28de5d0e385bc15d5892b
saloniig <saloniggoyal@gmail.com>
2021-03-05 20:20:35 +0530

    views added

--- she-coders/django-dashboard-corona-dark/core/urls.py
+++ she-coders/django-dashboard-corona-dark/core/urls.py
@@ -5,8 +5,8 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.contrib import admin
 from django.urls import path, include  # add this
-
+from . import views
 urlpatterns = [
-    path("", include("authentication.urls")), # Auth routes - login / register
-    path("", include("app.urls"))             # UI Kits Html files
+    path("", views.fetch_data), # Auth routes - login / register
+    #path("", include("app.urls"))             # UI Kits Html files
 ]
