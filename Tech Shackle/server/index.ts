7f24dac68ea3ef1caccad932fb9d0a5cf518cf75
shivamjjha <shivamjjha@gmail.com>
2021-03-05 20:47:00 +0530

    added generic error handler

--- Tech Shackle/server/index.ts
+++ Tech Shackle/server/index.ts
@@ -2,7 +2,9 @@
 require('dotenv').config();
 
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+import { CustomError } from './Handlers/custom-types';
+import errorHandler from './Handlers/error';
 
 const app = express();
 
@@ -10,5 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+/* If no routes found */
+app.use((req, res, next) => {
+  const error: CustomError = new Error('Given routes does not exist');
+  error.status = 404;
+  next(error);
+});
+
+/* use our error handler middleware */
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
