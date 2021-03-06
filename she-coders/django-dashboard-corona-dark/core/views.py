09464c66520c566fbcbe12534ef61fb289f774e5
saloniig <saloniggoyal@gmail.com>
2021-03-06 16:46:42 +0530

    add dynamic data

--- she-coders/django-dashboard-corona-dark/core/views.py
+++ she-coders/django-dashboard-corona-dark/core/views.py
@@ -1,7 +1,7 @@
 from .models import customer
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.db.models import Count
+from django.db.models import Count, Sum
 from django.contrib import messages
 from django.db.models.functions import ExtractHour
 from django.contrib.auth.decorators import login_required
@@ -16,6 +16,7 @@ def index(request):
             city1_customers = customer.objects.filter(city__city='ludhiana')
             city2_customers = customer.objects.filter(city__city='chandigarh')
             trending_items = customer.objects.values('item').annotate(count=Count('item')).order_by('item').reverse()
+            revenue_items = customer.objects.values('item').annotate(sum=Sum('price')).order_by('item').reverse()
             data=[]
             data.append(city1_customers.count())
             data.append(city2_customers.count())
@@ -29,6 +30,7 @@ def index(request):
                 'data':data,
                 'labels':labels,
                 'trending_items':trending_items,
+                'revenue_items':revenue_items,
                 'all_items':all_items
             
             }
@@ -42,7 +44,15 @@ def chart(request):
     citywise_customers = customer.objects.all().annotate(cityc=Count('city'))
     city1_customers = citywise_customers.filter(city__city='ludhiana')
     city2_customers = citywise_customers.filter(city__city='chandigarh')
+    city1item1 = customer.objects.filter(city__city='ludhiana', item__item='school bag' )
+    city1item2 = customer.objects.filter(city__city='ludhiana', item__item='hand bag' )
+    city1item3 = customer.objects.filter(city__city='ludhiana', item__item='travelling bag' )
+    city2item1 = customer.objects.filter(city__city='chandigarh', item__item='school bag' )
+    city2item2 = customer.objects.filter(city__city='chandigarh', item__item='hand bag' )
+    city2item3 = customer.objects.filter(city__city='chandigarh', item__item='travelling bag' )
+
     labels=[str(x)+':00 hours' for x in range(1,13)]
+    itemlabels=['school bag', 'hand bag', 'travelling bag']
     hourwise_customers1=city1_customers.annotate(hour=ExtractHour('timestamp')).values('hour').annotate(c=Count('id')).values('hour', 'c') 
     hourwise_customers2=city2_customers.annotate(hour=ExtractHour('timestamp')).values('hour').annotate(c=Count('id')).values('hour', 'c') 
     hourwisedata1=[x['c'] for x in hourwise_customers1]
@@ -50,7 +60,14 @@ def chart(request):
     context={
         'hourwise_customers1':hourwisedata1,
         'hourwise_customers2':hourwisedata2,
-        'labels':labels
+        'labels':labels,
+        'city1item1':city1item1.count(),
+        'city1item2': city1item2.count(),
+        'city1item3':city1item3.count(),
+        'itemlables':itemlabels,
+        'city2item1':city2item1.count(),
+        'city2item2': city2item2.count(),
+        'city2item3': city2item3.count()
     }
     return render(request, 'chartjs.html',context=context)
 
